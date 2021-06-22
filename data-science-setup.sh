cd057e076d59b5189ee56a57b8b11fc0066f6881
nicjlambert <lambni85@outlook.com>
2021-06-22 18:03:44 +1000

    added README and added git credentials to .sh

--- data-science-setup.sh
+++ data-science-setup.sh
@@ -1,3 +1,16 @@
-script = """#!/bin/bash
+#!/bin/bash
 
 set -euxo pipefail
+
+function configureGit()
+{
+	echo "##### Configure Git"
+        read -r -p "Enter Git User Name:" userName
+        git config --global user.name $userName
+        read -r -p "Enter GIT Email ID:" EmailID
+        git config --global user.email $EmailID
+	echo "Git configuration Detail:"
+	git config --list
+}
+
+configureGit
