1102f611609c23fc67049aa9be952f4a11d25e6c
nicjlambert <lambni85@outlook.com>
2022-12-14 21:24:43 +1100

    udpated code format and README text

--- README.md
+++ README.md
@@ -1,12 +1,32 @@
 # Data Science Setup on Ubuntu 22.04
 
-## Note to change ownership permissions
-`sudo chown -R username /path/to/file`
+This Bash script performs several tasks on an Ubuntu system, including:
 
-## Give execute permission to your script
-`chmod +x ./data-science-setup.sh`
+* Updating the system with the latest security patches and software updates.
+* Configuring Git with the user's name and email address.
+* Installing the R programming language and RStudio.
+* Installing Python and tools for managing Python packages, such as pip and virtual environments.
 
-## And to run the script
-`./data-science-setup.sh`
+## Usage
 
-Where `.` collapses the path to the current directory.
+To use this script, follow these steps:
+
+Download the script and save it on your Ubuntu system.
+* Make the script executable by running the following command: `chmod +x ./data-science-setup.sh`
+* Run the script with the following command: `./data-science-setup.sh`
+
+Where `.` collapses the path to the current directory. The script will perform the tasks in the order that they are listed above. You can comment out or remove any of the tasks that you don't want to run.
+
+## Requirements
+
+* A Ubuntu system.
+* `sudo` privileges on the system to run the script.
+
+## Troubleshooting
+If you encounter any errors or issues when running the script, please make sure that you have the required dependencies and sudo privileges on the system. You can also try running the script with the -x option to enable debugging, which will print out each command that is being executed, along with any error messages. This can help you to identify and troubleshoot any problems with the script.
+
+
+
+
+
+
--- data-science-setup.sh
+++ data-science-setup.sh
@@ -2,21 +2,26 @@
 
 set -euxo pipefail
 
+# Function to perform basic Ubuntu updates
 function systemBasicUpdate()
 {
 	echo "---- Basic ubuntu update"
-	# Update the apt package index and Upgrade the Ubuntu system
+	# Update the apt package index and upgrade the Ubuntu system
 	sudo apt-get update && sudo apt-get -y upgrade
 }
 
+# Function to configure Git
 function configureGit()
-# the user name/email is just used to track who made what changes
+# the user name/email is just used to track who made what changes	
 {
 	echo "---- Configure Git"
+
         read -r -p "Enter Git User Name:" userName
+		read -r -p "Enter GIT Email ID:" EmailID
+
         git config --global user.name $userName
-        read -r -p "Enter GIT Email ID:" EmailID
         git config --global user.email $EmailID
+
 	echo "Git configuration Detail:"
 	git config --list
 }
@@ -24,56 +29,54 @@ function configureGit()
 function installR()
 {
 
-# Install R programming language and RStudio
-sudo apt install dirmngr gnupg apt-transport-https ubuntu-keyring ca-certificates software-properties-common -y
+	# Install R programming language and RStudio
+	sudo apt install dirmngr gnupg apt-transport-https ubuntu-keyring ca-certificates software-properties-common -y
 
-# Import R GPG key
-wget -O- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo gpg --dearmor | sudo tee /usr/share/keyrings/cran.gpg
+	# Import R GPG key
+	wget -O- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo gpg --dearmor | sudo tee /usr/share/keyrings/cran.gpg
 
-# Import the CRAN repository
-echo deb [signed-by=/usr/share/keyrings/cran.gpg] https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/ | sudo tee /etc/apt/sources.list.d/cran.list
+	# Import the CRAN repository
+	echo deb [signed-by=/usr/share/keyrings/cran.gpg] https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/ | sudo tee /etc/apt/sources.list.d/cran.list
 
-sudo apt update
+	sudo apt update
 
-# Install R Programming Language
-echo "---- Install R"
-sudo apt install --no-install-recommends r-base r-base-dev build-essential libapparmor1 gdebi-core
+	# Install R Programming Language
+	echo "---- Install R"
+	sudo apt install --no-install-recommends r-base r-base-dev build-essential libapparmor1 gdebi-core
 
-# Download and install RStudio
-echo "---- Install RStudio"
-cd ~/tmp
-## version is current as at 12-07-2022
-wget https://download1.rstudio.org/desktop/jammy/amd64/rstudio-2022.07.0-548-amd64.deb
-sudo gdebi rstudio-2022.07.0-548-amd64.deb
-rm rstudio-2022.07.0-548-amd64.deb
+	# Download and install RStudio
+	echo "---- Install RStudio"
+	cd ~/tmp
+	## version is current as at 12-07-2022
+	wget https://download1.rstudio.org/desktop/jammy/amd64/rstudio-2022.07.0-548-amd64.deb
+	sudo gdebi rstudio-2022.07.0-548-amd64.deb
+	rm rstudio-2022.07.0-548-amd64.deb
 
-sudo adduser rstudio
+	sudo adduser rstudio
 
 }
-
-
 
 function installPython()
 {
-echo "---- Install Python"
+	echo "---- Install Python"
 
-# To manage software packages for Python
-sudo apt install -y python3-pip
+	# To manage software packages for Python
+	sudo apt install -y python3-pip
 
-# There are a few more packages and development tools to install 
-sudo apt install -y build-essential libssl-dev libffi-dev python3-dev
+	# There are a few more packages and development tools to install 
+	sudo apt install -y build-essential libssl-dev libffi-dev python3-dev
 
-# Virtual environments enable you to have an isolated space on your server for Python projects
-sudo apt install -y python3-venv
+	# Virtual environments enable you to have an isolated space on your server for Python projects
+	sudo apt install -y python3-venv
 
-# Activate virtual environment
-# source mypython/bin/activate
-# To decativate the virtual environment and use your original Python environment, simply type ‘deactivate’.
+	# Activate virtual environment
+	# source mypython/bin/activate
+	# To decativate the virtual environment and use your original Python environment, simply type ‘deactivate’.
 }
 
+# Call the functions
 
-
-systemBasicUpdate
+#systemBasicUpdate
 
 #configureGit
 
